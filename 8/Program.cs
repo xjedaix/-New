4d5f5f840553642e026a2625a2df59a4fbc30521
Evgeniy <evgeniyklimow@gmail.com>
2023-02-09 22:41:41 +0300

    add 8 project

---
+++ 8/Program.cs
@@ -0,0 +1,14 @@
+﻿// Задача 8: Напишите программу, которая на вход принимает число (N), а на выходе показывает все чётные числа от 1 до N.
+
+// 5 -> 2, 4
+// 8 -> 2, 4, 6, 8
+
+
+Console.Clear();
+Console.Write("Введите число: ");
+int number = int.Parse(Console.ReadLine());
+int i = -1 * number; // перемножение
+while (i <= number){
+    Console.WriteLine($"{i}");
+    i++; // i = i +1 
+}
