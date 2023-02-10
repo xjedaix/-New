bae93b401dd0e78ab28b10f2430691ba68e41017
Evgeniy <evgeniyklimow@gmail.com>
2023-02-10 22:38:31 +0300

    changes 9 project

--- 10/Program.cs
+++ 10/Program.cs
@@ -1,2 +1,19 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Задача 10: Напишите программу, которая принимает на вход трёхзначное число и на выходе показывает вторую цифру этого числа.
+
+// 456 -> 5
+// 782 -> 8
+// 918 -> 1
+
+Console.Clear();
+Console.Write("Введите трехзначное число: ");
+int number = int.Parse(Console.ReadLine());
+if (number < 99 || number > 1000)
+{
+        Console.WriteLine ("Введено не корректное число");
+        return;
+}
+number = number / 10;
+number = number % 10;
+Console.WriteLine($"Вторая цифра трехзначного числа = {number}");
+
+
--- 9/Program.cs
+++ 9/Program.cs
@@ -11,7 +11,7 @@ Console.Clear();
 int num = new Random().Next(10, 100);
 Console.WriteLine($"Num = {num}");
 int num1 = num / 10; //   / отбрасывает последние цифры числа, 10, 100 и т.д.
-int num2 = num % 10; // % возвращает последнюю цифру числа, 10, 100 и т.д.
+int num2 = num % 10; //   % возвращает последнюю цифру числа, 10, 100 и т.д.
 int max = num2;
 if (num1 > max){
     max = num1;
