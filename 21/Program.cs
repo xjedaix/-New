48f384f21ba39ffa9001e64b7308b911404e7ab9
Evgeniy <evgeniyklimow@gmail.com>
2023-02-12 15:24:57 +0300

    add projects

---
+++ 21/Program.cs
@@ -0,0 +1,23 @@
+﻿// Напишите программу,
+// которая принимает на вход координаты двух точек и находит
+// расстояние между ними в 2D пространстве.
+
+// A (3,6); B(2, 1)-> 5,09
+// A(7, -5); B(1, -1)-> 7,21
+//sqrt((3 - 2)^2 + (6 - 1)^2) = 5.09
+double s = Math.Sqrt(...);
+
+
+Console.Clear();
+Console.Write("Введите X1: ");
+double x1 = double.Parse(Console.ReadLine());
+Console.Write("Введите Y1: ");
+double y1 = double.Parse(Console.ReadLine());
+Console.Write("Введите X2: ");
+double x2 = double.Parse(Console.ReadLine());
+Console.Write("Введите Y2: ");
+double y2 = double.Parse(Console.ReadLine());
+
+double d = Math.Sqrt(Math.Pow(x1 - x2, 2) + Math.Pow(y1 - y2, 2));
+
+Console.WriteLine($"d={d:f3}");
---
+++ 22/Program.cs
@@ -0,0 +1,29 @@
+﻿// Напишите программу, которая принимает на вход число (N) и выдаёт таблицу квадратов чисел от 1 до N.
+
+// 5 -> 1, 4, 9, 16, 25.
+
+// 2 -> 1,4
+
+Console.Clear();
+Console.Write("Введите число N: ");
+int N = int.Parse(Console.ReadLine());
+int i = 1;
+int m = N; // m = -10
+if (N < 0)
+{
+    i = N; //i = -10
+    m = -N;// m = 10
+    N = -1; // N = -1
+    
+
+}
+int[] result = new int[m];
+for (int j = 0; i <= N; i++)
+{
+    result[j] = i * i;
+    j++;
+}
+for (int j = 0; j < m; j++)
+{
+    Console.Write($"{result[j]} ");
+}
