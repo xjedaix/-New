df98573beb44cbcbb76e75c61e7e774f98638df1
Evgeniy <evgeniyklimow@gmail.com>
2023-02-13 23:46:27 +0300

    add 30 project

--- 30/Program.cs
+++ 30/Program.cs
@@ -1,2 +1,14 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿//30 Напишите программу, которая выводит массив из 8 элементов,
+//заполненный нулями и единицами в случайном порядке.
+
+//[1,0,1,1,0,1,0,0]
+
+// a = new Random().Next()
+
+Console.Clear();
+Console.Write("Введите число: ");
+int num = int.Parse(Console.ReadLine());
+int[] array = GetBinArray(num);
+Console.Write($"[{String.Join(", ", array)}]");
+
+int []
